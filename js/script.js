5a277549221d6ea06d9cc0d441a682e9925d38c0
Stefanus Bernard Melkisedek <stefanusbernardmelkisedek@gmail.com>
2023-11-09 16:26:50 +0700

    add script for phone (not active yet)

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const phoneInputField = document.querySelector("#phone");
+    const phoneInput = window.intlTelInput(phoneInputField, {
+      utilsScript:
+        "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+    });
