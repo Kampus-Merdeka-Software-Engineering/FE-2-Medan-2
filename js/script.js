ced569fae3187683f6a7bc0f1a1a986761d5a1aa
Stefanus Bernard Melkisedek <stefanusbernardmelkisedek@gmail.com>
2023-11-11 17:03:56 +0700

    Add js to menubar

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+
+const menu = document.querySelector(".menu-icon")
+menu.addEventListener("click", () => {
+  const navbar = document.querySelector(".navbar")
+  navbar.classList.toggle("hidden")
+  navbar.classList.toggle("show")
+})
+
 // Phone number
 
 const phoneInputField = document.querySelector("#phone");
