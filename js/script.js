fc266c83f36ef07332bec5d5add41a714069919a
Stefanus Bernard Melkisedek <stefanusbernardmelkisedek@gmail.com>
2023-11-11 17:06:49 +0700

    format code

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
+// menu-bar icon
 
-const menu = document.querySelector(".menu-icon")
+const menu = document.querySelector(".menu-icon");
 menu.addEventListener("click", () => {
-  const navbar = document.querySelector(".navbar")
-  navbar.classList.toggle("hidden")
-  navbar.classList.toggle("show")
-})
+  const navbar = document.querySelector(".navbar");
+  navbar.classList.toggle("hidden");
+  navbar.classList.toggle("show");
+});
 
 // Phone number
 
 const phoneInputField = document.querySelector("#phone");
-   const phoneInput = window.intlTelInput(phoneInputField, {
-     utilsScript:
-       "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
-   });
+const phoneInput = window.intlTelInput(phoneInputField, {
+  utilsScript:
+    "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+});
 
 // Review slide
-
