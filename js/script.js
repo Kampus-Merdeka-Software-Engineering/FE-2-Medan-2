006af5269df8e036f32f39eef400750d439bb2f0
Stefanus Bernard Melkisedek <stefanusbernardmelkisedek@gmail.com>
2023-11-13 15:50:01 +0700

    add script to home section

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+// menu video button
+
+let videoBtn = document.querySelectorAll(".vid-btn");
+videoBtn.forEach(btn => {
+  btn.addEventListener("click", () => {
+      document.querySelector('.controls .active').classList.remove('active');
+      btn.classList.add('active');
+      let src = btn.getAttribute('data-src');
+      document.querySelector('#video-slider').src = src; 
+  });
+});
+
 // menu-bar icon
 
 const menu = document.querySelector(".menu-icon");
