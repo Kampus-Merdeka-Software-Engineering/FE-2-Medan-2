2414e4d879efc7fc23fce16115f208b525b4cbfb
Stefanus Bernard Melkisedek <stefanusbernardmelkisedek@gmail.com>
2023-11-15 22:13:12 +0700

    fix book ticket, check ticket, and others

--- js/script.js
+++ js/script.js
@@ -1,18 +1,19 @@
 // menu video button
 
 let videoBtn = document.querySelectorAll(".vid-btn");
-videoBtn.forEach(btn => {
+videoBtn.forEach((btn) => {
   btn.addEventListener("click", () => {
-      document.querySelector('.controls .active').classList.remove('active');
-      btn.classList.add('active');
-      let src = btn.getAttribute('data-src');
-      document.querySelector('#video-slider').src = src; 
+    document.querySelector(".controls .active").classList.remove("active");
+    btn.classList.add("active");
+    let src = btn.getAttribute("data-src");
+    document.querySelector("#video-slider").src = src;
   });
 });
 
 // menu-bar icon
 
 const menu = document.querySelector(".menu-icon");
+console.log(menu);
 menu.addEventListener("click", () => {
   const navbar = document.querySelector(".navbar");
   navbar.classList.toggle("hidden");
@@ -33,13 +34,10 @@ let book_url = document.getElementById("book_url");
 
 book_url.addEventListener("click", () => {
   window.location.href = "book-ticket.html";
-})
+});
 
-document.getElementById("book").onclick = 
-  function () {
-    window.location.href = "book.html";
-  }
-
+document.getElementById("book").onclick = function () {
+  window.location.href = "book.html";
+};
 
 // Redirect from book section to check ticket section
-
