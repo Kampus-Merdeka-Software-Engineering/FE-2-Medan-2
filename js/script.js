957df8a414463641f10c7d5bce6cc31f2bb642c7
Stefanus Bernard Melkisedek <stefanusbernardmelkisedek@gmail.com>
2023-11-20 14:18:30 +0700

    Add script redirect btn book to ticket-sect

--- js/script.js
+++ js/script.js
@@ -62,4 +62,53 @@ document.getElementById("book").onclick = function () {
   window.location.href = "book.html";
 };
 
-// ! Redirect from book section to check ticket section
+// ! Redirect from book section to check ticket section after user receive ticket number
+
+document
+  .querySelector("#book-ticket-btn")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+
+    let ticketNumber = Math.floor(Math.random() * 1000000); // Generate a random ticket number
+    alert(`Your ticket number is ${ticketNumber}`);
+
+    let formData = {
+      ticketNumber: ticketNumber,
+      name: document.getElementById("name").value,
+      whereTo: document.getElementById("where-to").value,
+      email: document.getElementById("email").value,
+      quantity: document.getElementById("quantity").value,
+      phone: document.getElementById("phone").value,
+      arrival: document.querySelector(
+        'input[type="datetime-local"]:nth-child(1)'
+      ).value,
+      leaving: document.querySelector(
+        'input[type="datetime-local"]:nth-child(2)'
+      ).value,
+    };
+
+    localStorage.setItem("formData", JSON.stringify(formData)); // Store the form data in local storage
+
+    window.location.href = "check-ticket.html"; // Redirect to the check-ticket.html page
+  });
+
+window.onload = function () {
+  let formData = JSON.parse(localStorage.getItem("formData")); // Retrieve the form data from local storage
+
+  document.querySelector(".ticket-column:nth-child(1) input").value =
+    formData.ticketNumber;
+  document.querySelector(".ticket-column:nth-child(2) input").value =
+    formData.name;
+  document.querySelector(".ticket-column:nth-child(3) input").value =
+    formData.email;
+  document.querySelector(".ticket-column:nth-child(4) input").value =
+    formData.phone;
+  document.querySelector(".ticket-column:nth-child(5) input").value =
+    formData.whereTo;
+  document.querySelector(".ticket-column:nth-child(6) input").value =
+    formData.quantity;
+  document.querySelector(".ticket-column:nth-child(7) input").value =
+    formData.arrival;
+  document.querySelector(".ticket-column:nth-child(8) input").value =
+    formData.leaving;
+};
