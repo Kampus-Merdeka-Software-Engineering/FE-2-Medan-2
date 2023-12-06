8f86897c9f93dd6547801575a79763dbd77fb2e7
Stefanus Bernard Melkisedek <stefanusbernardmelkisedek@gmail.com>
2023-12-06 15:34:38 +0700

    Add book ticket form and API integration

--- js/book-ticket.js
+++ js/book-ticket.js
@@ -15,3 +15,62 @@ const phoneInput = window.intlTelInput(phoneInputField, {
   utilsScript:
     "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
 });
+
+// ! Defines the API URL and creates a function called bookTicketForm.
+
+const API_URL = "https://dull-jade-parrot-tam.cyclic.app";
+
+const bookTicketForm = async () => {
+  try {
+    const respond = await fetch(`${API_URL}/book-ticket`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: document.querySelector("#name").value,
+        email: document.querySelector("#email").value,
+        phoneNumber: document.querySelector("#phone").value,
+        message: document.querySelector("#message").value,
+        destination: document.querySelector("#destination").value,
+        quantity: document.querySelector("#quantity").value,
+        arrivalDate: document.querySelector("#arrivalDate").value,
+        leavingDate: document.querySelector("#leavingDate").value,
+      }),
+    }).then((res) => res.json());
+    console.log(respond);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// Show the ticket number in an alert when the user clicks the Book Now button.
+document
+  .querySelector("#bookTicketBtn")
+  .addEventListener("click", async function () {
+    try {
+      const response = await bookTicketForm();
+      if (response && response.ticketNumber) {
+        Swal.fire({
+          icon: "success",
+          title: "Booking Succcesfull",
+          text: `Your ticket number is: ${response.ticketNumber}`,
+        }).then(() => {
+          window.location.href = `${API_URL}/check-ticket}`;
+        });
+      } else {
+        Swal.fire({
+          icon: "error",
+          title: "Booking Failed",
+          text: "Failed to book ticket. Please try again.",
+        });
+      }
+    } catch (error) {
+      console.error(error);
+      Swal.fire({
+        icon: "error",
+        title: "Error",
+        text: "An error occured. Please try again.",
+      });
+    }
+  });
