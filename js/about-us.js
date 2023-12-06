c4560e8a451363189456fbe0e6e3bb8e48f2dd70
Stefanus Bernard Melkisedek <stefanusbernardmelkisedek@gmail.com>
2023-12-06 16:38:09 +0700

    Add active-link class to "About Us" link in navbar

--- js/about-us.js
+++ js/about-us.js
@@ -7,3 +7,17 @@ menu.addEventListener("click", () => {
   navbar.classList.toggle("hidden");
   navbar.classList.toggle("show");
 });
+
+// ! active-link navbar
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Get the "About Us" link
+  const aboutUsLink = Array.from(
+    document.querySelectorAll(".navbar li a")
+  ).find((a) => a.textContent === "About Us");
+
+  // Add the active class to the "About Us" link
+  if (aboutUsLink) {
+    aboutUsLink.classList.add("active-link");
+  }
+});
