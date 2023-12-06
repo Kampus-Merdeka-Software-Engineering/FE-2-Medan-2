a45561a467d7472ea8aadcebb5d9db1461310a4c
Stefanus Bernard Melkisedek <stefanusbernardmelkisedek@gmail.com>
2023-12-06 16:37:46 +0700

    Add active-link functionality to navbar

--- js/index.js
+++ js/index.js
@@ -8,6 +8,44 @@ menu.addEventListener("click", () => {
   navbar.classList.toggle("show");
 });
 
+// ! active-link navbar
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Get all the navigation links
+  const navLinks = document.querySelectorAll(".navbar li a");
+
+  // Add an event listener to each link
+  navLinks.forEach((link) => {
+    link.addEventListener("click", function (event) {
+      // Prevent the default link behavior
+      event.preventDefault();
+
+      // Remove the active class from all links
+      navLinks.forEach((link) => link.classList.remove("active-link"));
+
+      // Add the active class to the clicked link
+      this.classList.add("active-link");
+
+      // Store the clicked link in the local storage
+      localStorage.setItem("activeLink", this.href);
+
+      // Navigate to the new page
+      window.location.href = this.href;
+    });
+  });
+
+  // Get the active link from the local storage
+  const activeLink = localStorage.getItem("activeLink");
+
+  // If there is an active link, add the active class to it
+  if (activeLink) {
+    const link = document.querySelector(`.navbar li a[href="${activeLink}"]`);
+    if (link) {
+      link.classList.add("active-link");
+    }
+  }
+});
+
 // ! video button
 
 let videoBtn = document.querySelectorAll(".vid-btn");
