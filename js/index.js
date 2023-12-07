f7550d36718b916cd0addbf2dfb0b7b31078e2b1
Stefanus Bernard Melkisedek <stefanusbernardmelkisedek@gmail.com>
2023-12-07 21:52:44 +0700

    Refactor navigation and video slider event listeners

--- js/index.js
+++ js/index.js
@@ -11,33 +11,24 @@ menu.addEventListener("click", () => {
 // ! active-link navbar
 
 document.addEventListener("DOMContentLoaded", function () {
-  // Get all the navigation links
   const navLinks = document.querySelectorAll(".navbar li a");
 
-  // Add an event listener to each link
   navLinks.forEach((link) => {
     link.addEventListener("click", function (event) {
-      // Prevent the default link behavior
       event.preventDefault();
 
-      // Remove the active class from all links
       navLinks.forEach((link) => link.classList.remove("active-link"));
 
-      // Add the active class to the clicked link
       this.classList.add("active-link");
 
-      // Store the clicked link in the local storage
       localStorage.setItem("activeLink", this.href);
 
-      // Navigate to the new page
       window.location.href = this.href;
     });
   });
 
-  // Get the active link from the local storage
   const activeLink = localStorage.getItem("activeLink");
 
-  // If there is an active link, add the active class to it
   if (activeLink) {
     const link = document.querySelector(`.navbar li a[href="${activeLink}"]`);
     if (link) {
@@ -68,12 +59,12 @@ videoBtn.forEach((btn) => {
       let src = btn.getAttribute("data-src");
       videoSlider.src = src;
       videoSlider.style.opacity = 1;
-    }, 500); // The duration here should match the transition duration in CSS
+    }, 500);
 
     // Add 'fade-out' class on click and then remove it after the transition ends
     btn.classList.add("fade-out");
     setTimeout(() => {
       btn.classList.remove("fade-out");
-    }, 250); // The duration here should match the transition duration in CSS
+    }, 250);
   });
 });
