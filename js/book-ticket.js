2773230ed7da3f0ba4a2620e8e215ba682536262
Stefanus Bernard Melkisedek <stefanusbernardmelkisedek@gmail.com>
2023-12-08 22:43:15 +0700

    feat: add loader and activel ink feature to Book Ticket page

--- js/book-ticket.js
+++ js/book-ticket.js
@@ -102,3 +102,15 @@ document
       });
     }
   });
+
+// ! Provides a loader for the page
+
+window.addEventListener("load", () => {
+  const loader = document.querySelector(".loader");
+
+  loader.classList.add("loader--hidden");
+
+  loader.addEventListener("transitionend", () => {
+    document.body.removeChild(loader);
+  });
+});
