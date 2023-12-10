d4583cfdc5a223f1d8ba82318ce259e5f6a19d7a
Stefanus Bernard Melkisedek <stefanusbernardmelkisedek@gmail.com>
2023-12-10 16:36:33 +0700

    refactor(about-us.js): implement best practices for code writing

--- src/js/about-us.js
+++ src/js/about-us.js
@@ -1,50 +1,39 @@
-// ! menu-bar icon
+// Handles the functionality of the menu bar icon
+function handleMenuBar() {
+  const menuIcon = document.querySelector(".menu-icon");
 
-// Select the menu icon element
-const menu = document.querySelector(".menu-icon");
-
-// Log the selected menu element for debugging
-console.log(menu);
-
-// Add a click event listener to the menu icon
-menu.addEventListener("click", () => {
-  // Select the navbar element
-  const navbar = document.querySelector(".navbar");
-
-  // Toggle visibility of the navbar on click
-  navbar.classList.toggle("hidden");
-
-  // Toggle the 'show' class to animate the navbar
-  navbar.classList.toggle("show");
-});
-
-// ! active-link navbar
-
-// Wait for the DOM content to load
-document.addEventListener("DOMContentLoaded", function () {
-  // Select the 'About Us' link from the navigation bar
-  const aboutUsLink = Array.from(document.querySelectorAll(".navbar li a"))
-                           .find((a) => a.textContent === "About Us");
-
-  // If the 'About Us' link exists, add the 'active-link' class to it
-  if (aboutUsLink) {
-    aboutUsLink.classList.add("active-link");
-  }
-});
-
-// ! Provides a loader for the page
-
-// On page load
-window.addEventListener("load", () => {
-  // Select the loader element
-  const loader = document.querySelector(".loader");
-
-  // Hide the loader
-  loader.classList.add("loader--hidden");
-
-  // On loader transition end
-  loader.addEventListener("transitionend", () => {
-    // Remove the loader from the DOM
-    document.body.removeChild(loader);
+  menuIcon.addEventListener("click", () => {
+    const navbar = document.querySelector(".navbar");
+    navbar.classList.toggle("hidden");
+    navbar.classList.toggle("show");
   });
-});
+}
+
+// Handles the functionality of the active link in the navbar
+function handleActiveLink() {
+  document.addEventListener("DOMContentLoaded", () => {
+    const navLinks = document.querySelectorAll(".navbar li a");
+    const aboutUsLink = Array.from(navLinks).find((link) => link.textContent === "About Us");
+
+    if (aboutUsLink) {
+      aboutUsLink.classList.add("active-link");
+    }
+  });
+}
+
+// Handles the functionality of the page loader
+function handlePageLoader() {
+  window.addEventListener("load", () => {
+    const loader = document.querySelector(".loader");
+    loader.classList.add("loader--hidden");
+
+    loader.addEventListener("transitionend", () => {
+      document.body.removeChild(loader);
+    });
+  });
+}
+
+// Call the functions
+handleMenuBar();
+handleActiveLink();
+handlePageLoader();
